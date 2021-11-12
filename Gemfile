a94c7762a04b6dbf90701005ebd04314070f6510
Devin Pile <devinpile@gmail.com>
2021-11-12 13:09:58 -0700

    Add changes for initial setup

--- Gemfile
+++ Gemfile
@@ -36,10 +36,12 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'shoulda-matchers'
   gem 'rspec-rails'
+  gem 'simplecov'
   gem 'capybara'
   gem 'launchy'
-  gem 'shoulda-matchers'
+  gem 'orderly'
   gem 'pry'
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-hospital = Hospital.create(name: "Grey Sloan Memorial Hospital")
-dr_bailey = hospital.doctors.create(name: "Miranda Bailey", specialty: "General Surgery", education: "Stanford University")
+# hospital = Hospital.create(name: "Grey Sloan Memorial Hospital")
+# dr_bailey = hospital.doctors.create(name: "Miranda Bailey", specialty: "General Surgery", education: "Stanford University")
