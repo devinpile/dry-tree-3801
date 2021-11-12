f3bbae497605bdacba24a64324b6fa95bf9c672d
Devin Pile <devinpile@gmail.com>
2021-11-12 14:14:41 -0700

    Add changes to implement doctor show page

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -2,5 +2,6 @@ class DoctorsController < ApplicationController
   def show
     @hospital = Hospital.find(params[:id])
     @doctor = Doctor.find(params[:id])
+    @patients = Patient.all
   end
 end
